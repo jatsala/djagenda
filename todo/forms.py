1156988b44f53188960e68cb1da36700870508b7
jatsala <jatsalaberria@gmail.com>
2024-08-28 10:24:24 -0400

    first commit

---
+++ todo/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm, DateInput
+from .models import Todo
+
+class TodoForm(ModelForm):
+    class Meta:
+        model = Todo
+        # fields = '__all__'
+        exclude = ('date',)
+        widgets = {
+            'estimated_end': DateInput(attrs={'type':'date'})
+        }
+
